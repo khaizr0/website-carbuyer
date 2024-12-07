c8ac7d51a69ee394f4d3cc714bb1838b22b7ada8
Cao Phan Khải <54585187+khaizr0@users.noreply.github.com>
2024-12-08 01:40:14 +0700

    update

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const booking = require('./routes/DatLichKHRoute');
 const bookingRoutes = require('./routes/Booking');
 const myUserRoute = require('./routes/MyUserRoute');
 const userRoute = require('./routes/UserRoute');
+const otherRoutes = require('./routes/OtherRoute');
 const path = require('path');
-const multer = require('multer');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +25,7 @@ app.use('/config', express.static(path.join(__dirname, 'config')));
 
 // Routes
 app.use('/', authRoutes);
+app.use('/', otherRoutes);
 app.use('/api/my/user', myUserRoute);
 app.use('/api/user', userRoute);
 app.use('/employee', employeeRoutes);
@@ -32,24 +33,6 @@ app.use('/product', productRoutes);
 app.use('/news', newsRoutes);
 app.use('/booking', booking);
 app.use('/lichHen', bookingRoutes);
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'Home.html'));
-});
-app.get('/xedangban', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'productOption.html'));
-});
-app.get('/phukien', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'otherProduct.html'));
-});
-
-app.get('/chitietxe', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'Product-detail.html'));
-});
-app.get('/chitietphukien', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'other-detail.html'));
-});
-
 
 // 404 errors
 app.use((req, res) => {
---
+++ routes/OtherRoute.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const viewsPath = path.join(__dirname, '../views'); 
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'Home.html'));
+});
+
+router.get('/xedangban', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'productOption.html'));
+});
+
+router.get('/phukien', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'otherProduct.html'));
+});
+
+router.get('/chitietxe', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'Product-detail.html'));
+});
+
+router.get('/chitietphukien', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'other-detail.html'));
+});
+
+module.exports = router;
